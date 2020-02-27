544e970dced798dc0fc4f8bb2a37927884fd0370
Akira13641 <operator97@gmail.com>
2020-02-27 15:26:58 -0500

    Fix script

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,5 @@
 set -e
 cd ./source
-if [ "$CC" = "gcc" ]; then g++ -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp
-if [ "$CC" = "clang" ]; then clang++ -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp
+if [ "$CC" = "gcc" ]; then g++ -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ; fi
+if [ "$CC" = "clang" ]; then clang++ -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ; fi
 time ./binarytrees_benchmark 21
