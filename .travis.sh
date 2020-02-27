0a312c11c7817929ee35a51571661c07c71ca84e
Akira13641 <operator97@gmail.com>
2020-02-27 15:24:52 -0500

    Adjust script

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,5 @@
 set -e
 cd ./source
-g++ -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp
+if [ "$CC" = "gcc" ]; then g++ -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp
+if [ "$CC" = "clang" ]; then clang++ -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp
 time ./binarytrees_benchmark 21
