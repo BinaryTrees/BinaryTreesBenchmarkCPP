ada1c2d1ba2421a40c9e2d297ae51e9fa04e5c53
Akira13641 <operator97@gmail.com>
2020-02-27 21:02:45 -0500

    Fix script

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,9 @@
 set -e
 cd ./source
-if ["$CC" = "gcc"]; then 
-  g++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp;
+if [ "$CC" = "gcc" ]; then 
+  g++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ;
 fi
-if ["$CC" = "clang"]; then
-  clang++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp;
+if [ "$CC" = "clang" ]; then
+  clang++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ;
 fi
 time ./binarytrees_benchmark 21
