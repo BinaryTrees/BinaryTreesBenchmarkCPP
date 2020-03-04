048094d2a28dc8186b38591536ad1d286d85f042
Akira13641 <operator97@gmail.com>
2020-03-04 09:46:49 -0500

    Use `-std=c++14`

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,9 @@
 set -e
 cd ./source
 if [ "$CC" = "gcc" ]; then 
-  g++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ;
+  g++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=c++14 -o ./binarytrees_benchmark ./binarytrees.cpp ;
 fi
 if [ "$CC" = "clang" ]; then
-  clang++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=gnu++11 -o ./binarytrees_benchmark ./binarytrees.cpp ;
+  clang++ -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=c++14 -o ./binarytrees_benchmark ./binarytrees.cpp ;
 fi
 time ./binarytrees_benchmark 21
