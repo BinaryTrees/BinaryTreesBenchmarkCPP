023f247c440056482386bff1b8ddf2e8e1e0fa09
Akira13641 <operator97@gmail.com>
2020-09-15 11:24:33 -0400

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -7,6 +7,6 @@ time ./binarytrees_benchmark 21
 rm -rf ./binarytrees_benchmark
 export CC=clang-10 && export CXX=clang++-10
 echo $CXX --version
-$CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=c++14 -o ./binarytrees_benchmark ./binarytrees.cpp ;
+$CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=c++14 -o ./binarytrees_benchmark ./binarytrees.cpp ;
 time ./binarytrees_benchmark 21
 rm -rf ./binarytrees_benchmark
