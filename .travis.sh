dddd105fb7379d78186ae8fa21b41acf357202aa
Akira13641 <operator97@gmail.com>
2021-03-16 17:31:47 -0400

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -14,11 +14,11 @@ rm -rf ./binarytrees
 echo "Testing the BenchmarksGame implementation"
 export CC=gcc-10 && export CXX=g++-10
 $CXX --version
-$CXX -DNDEBUG -fopenmp -g0 -march=native -O3 -std=c++17 -ltbb -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
+$CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -g0 -march=native -O3 -std=c++17 -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
 time ./binarytrees_benchmarksgame 21
 rm -rf ./binarytrees_benchmarksgame
 export CC=clang-10 && export CXX=clang++-10
 $CXX --version
-$CXX -DNDEBUG -fopenmp=libgomp -g0 -march=native -O3 -std=c++17 -ltbb -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
+$CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -g0 -march=native -O3 -std=c++17 -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
 time ./binarytrees_benchmarksgame 21
 rm -rf ./binarytrees_benchmarksgame
