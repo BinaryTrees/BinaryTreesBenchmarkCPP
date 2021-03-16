0cf0eb231123efd74f09ba2007788db4b4f12e49
Akira13641 <operator97@gmail.com>
2021-03-16 17:16:11 -0400

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,23 +1,23 @@
 set -e
 cd ./source
 echo "Testing this implementation"
-export CC=gcc && export CXX=g++
+export CC=gcc-10 && export CXX=g++-10
 $CXX --version
 $CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=c++17 -o ./binarytrees ./binarytrees.cpp ;
 time ./binarytrees 21
 rm -rf ./binarytrees
-export CC=clang-11 && export CXX=clang++-11
+export CC=clang-10 && export CXX=clang++-10
 $CXX --version
 $CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=c++17 -o ./binarytrees ./binarytrees.cpp ;
 time ./binarytrees 21
 rm -rf ./binarytrees
 echo "Testing the BenchmarksGame implementation"
-export CC=gcc && export CXX=g++
+export CC=gcc-10 && export CXX=g++-10
 $CXX --version
 $CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp -march=native -O3 -std=c++17 -ltbb -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
 time ./binarytrees_benchmarksgame 21
 rm -rf ./binarytrees_benchmarksgame
-export CC=clang-11 && export CXX=clang++-11
+export CC=clang-10 && export CXX=clang++-10
 $CXX --version
 $CXX -DNDEBUG -fno-exceptions -fno-rtti -fopenmp=libgomp -march=native -O3 -std=c++17 -ltbb -o ./binarytrees_benchmarksgame ./binarytrees_benchmarksgame.cpp ;
 time ./binarytrees_benchmarksgame 21
